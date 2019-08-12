b119ce0c657d7565617275e0c7ff375a8e1ae59c
singhvirendra18gmailcom <singh.virendra18@gmail.com>
2019-08-12 08:04:03 +0530

    commit and push first program

---
+++ AdvanceMultithreading/src/test/TestClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author singh
+ *
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Github...");
+
+	}
+
+}
